8af600f889ab8f483bb9be3e7e5dff9b6a27e0da
chinnapareddy <chinna.aduri@gmail.com>
2023-10-23 04:22:03 -0700

    Terraform Elasticache

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ resource "aws_elasticache_subnet_group" "main" {
 }
 
 resource "aws_elasticache_parameter_group" "main" {
-  name   = "cache-params"
+  name   = "${local.name_prefix}-subnet-group"
   family = var.family
-  tags = merge(local.tags, {Name = "${local.name_prefix}-subnet-group"})
+  tags = merge(local.tags, {Name = "${local.name_prefix}-pg"})
 }
 
 resource "aws_security_group" "main" {
