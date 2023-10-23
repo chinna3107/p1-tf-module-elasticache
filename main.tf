792d95415286dc3054ea3cad1edcee7e144434c0
chinnapareddy <chinna.aduri@gmail.com>
2023-10-23 04:17:29 -0700

    Terraform Elasticache

--- main.tf
+++ main.tf
@@ -38,9 +38,10 @@ resource "aws_elasticache_cluster" "main" {
   engine               = var.engine
   node_type            = var.node_type
   num_cache_nodes      = var.num_cache_nodes
-  parameter_group_name = var.parameter_group_name
+  parameter_group_name = aws_elasticache_parameter_group.main.name
   engine_version       = var.engine_version
   port                 = var.port
   subnet_group_name    = aws_elasticache_subnet_group.main.name
-  security_group_ids = [aws_security_group.main.id]
+  security_group_ids   = [aws_security_group.main.id]
+  tags = merge(local.tags, {Name = "${local.name_prefix}-cluster"})
 }
